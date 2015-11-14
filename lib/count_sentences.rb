9aa2058c80821d0a0071190e085dc84b5d04897c
jakebrady5 <jakebrady5@gmail.com>
2015-11-14 12:16:09 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/[.?!]/)
+    #could a better regex eliminate line below?
+    count.delete_if{|a| a == ""}
+    count.length
   end
 end
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+complex_string.count_sentences
