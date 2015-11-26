d07ac2e90b40ef36ceb328c18c6d282ccc11e3d8
matpa <mateipopa@icloud.com>
2015-11-25 18:15:25 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    i=0
+    sentence_count=0
+    while i < self.length
+      if self[i]=="." && self[i+1]==" " || self[i]=="!" || self[i]=="?"
+      sentence_count+=1
+      end
+      i+=1
+    end
+  return sentence_count
+end
 
-  end
 end
