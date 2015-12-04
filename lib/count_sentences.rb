7e52334e43fca45608f321eff501d23cb4353e8c
authorbeard <msnolan@gmail.com>
2015-12-04 11:50:13 -0500

    All tests passing

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self.match(/\b.*\.$/) ? true : false
+    self.end_with?(".")
   end
 
   def question?
-
+    # self.match(/\?$/) ? true : false
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    # self.match(/\!/) ? true : false
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[?.!]/).grep(/\w+\b/).size
   end
 end
