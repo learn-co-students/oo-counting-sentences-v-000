2724c039a498c20c646b282db33319dc4ca61481
Karuna <imkaruna@gmail.com>
2015-12-08 16:00:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    sentences_array =[]
+    if self.include?("?") || self.include?(".") || self.include?("!")
+    sentences_array = self.split("?")
+    sentences_array << self.split("!")
+    sentences_array << self.split(".")  
+    end
+    return sentences_array.length
   end
 end
