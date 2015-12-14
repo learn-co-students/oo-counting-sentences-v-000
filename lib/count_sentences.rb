13259d1dc78f64dce6e747b4aa241317b4ad4a61
Mitul Mistry <mitul.mistry1@gmail.com>
2015-12-14 23:52:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    x = self.split(/[.!?]/).reject { |s| s.empty? }
+    x.length
+    #x.each do |i|
+      #if i.length > 0
+    #end
   end
 end
