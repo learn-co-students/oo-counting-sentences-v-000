e6f58de897b546aed3676e16997424fa0237a2cf
James Novak <jamesnovak90@gmail.com>
2015-12-16 13:29:47 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).delete_if {|str| str.empty?}.count
   end
 end
+
+#complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+#complex_string.count_sentences
