28ad93d48f850e74fc657df0805bcbd0d360c71b
jsgoldb <jgoldb@yahoo.com>
2015-12-23 17:14:19 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/\.\z/) ? true : false
   end
 
   def question?
-
+    self.match(/\?\z/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/\!\z/) ? true : false
   end
 
   def count_sentences
-
+    self.split(/[\.\?!]/).reject {|string| string.empty?}.count
   end
+
 end
