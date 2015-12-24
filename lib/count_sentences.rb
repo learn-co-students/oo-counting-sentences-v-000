4afba272fc9f85412688dcf00ec8768ea5a3a432
eric-an <erican@me.com>
2015-12-23 18:36:15 -0800

    define instance methods on a class utilizing self keyword and regex

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    modified = self.split(/[!|.|?]/)
+    sentences_array = modified.reject { |i| i.empty?} 
+    sentences_array.count
   end
 end
