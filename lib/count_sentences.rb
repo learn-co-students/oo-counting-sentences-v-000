9baf4820001d1eff901842bbc516a71945ff0579
John Anthony <askinganthony@gmail.com>
2016-01-01 00:10:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.scan(/[?!.]/).count != 0
   end
 
   def question?
-
+    self.scan(/[?]/).count != 0
   end
 
   def exclamation?
-
+    self.scan(/[!]/).count != 0
   end
 
   def count_sentences
-
+    self.scan(/\w[?.!]/).count
   end
 end
