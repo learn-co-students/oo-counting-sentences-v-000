9952b8ca997350300f8772ec4519a39cb4d36aba
jmheyd <heydjm@comcast.net>
2016-01-26 13:47:14 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	#string.sentence? returns true if string ends in a period
+  	if self.end_with?(".") then 
+  		true
+  	else
+  		false
+  	end    
   end
 
   def question?
+  		#string.sentence? returns true if string ends in a question mark
+  	if self.end_with?("?") then 
+  		true
+  	else
+  		false
+  	end 
 
   end
 
   def exclamation?
-
+	#string.sentence? returns true if string ends in an exclamation mark
+  	if self.end_with?("!") then 
+  		true
+  	else
+  		false
+  	end 
   end
 
   def count_sentences
-
+  	self.split(%r{[.?!]\s|[.?!]"}).count
   end
 end
