7934cf688dbbc63fe22ca432cc288209a42d9c35
Ben C <bcwong1@ualberta.ca>
2016-01-26 20:43:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,21 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    if self.include?('.') then true else false
+    if self.end_with?('.') then true else false
   end
 end
 
   def question?
-    if self.include?('?') then true else false
+    if self.end_with?('?') then true else false
     end
   end
 
   def exclamation?
-    if self.include?('!') then true else false
+    if self.end_with?('!') then true else false
     end
   end
 
   def count_sentences
     self.squeeze('.!?').count('.!?')
   end
+
 end
