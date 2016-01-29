2d31024ba07a0ad4130a157e463d6bdee2917e6d
zachnewburgh <zach.newburgh@gmail.com>
2016-01-29 14:17:39 -0500

    split/count solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,25 @@ class String
   end
 
   def count_sentences
-    self.scan(/[!?.]\s/).length + self.scan(/[!?.]\z/).length
-  end
+    #REGEX SOLUTION #
+    # self.scan(/[!?.]\s/).length + self.scan(/[!?.]\z/).length
+    
+    # SPLIT/COUNT SOLUTION #
+    sum = 0
+    period = self.split(". ").count
+    exclaim = self.split("! ").count
+    question = self.split("? ").count
 
-end
+    if period > 0 # if it can be split by periods, do so.
+      sum += period # add it to the sum
+      if exclaim > 1 # if splitting by ! yields an array of 2 or more
+        sum+= exclaim - 1 # subtract the initial split and add the rest to the sum
+      end
+      if question > 1 # if splitting by ? yields an array of 2 or more
+        sum += question - 1 # subtract the initial split and add the rest to the sum
+      end
+    else
+      sum = 0 # return a sum of 0 if the string is blank
+    end   
+    sum
+  end
