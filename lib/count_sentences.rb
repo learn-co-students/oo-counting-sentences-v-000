bfadab0cb602cf26fff96baeb76cde5934b6f69b
Lady Codington <myemailforcoding@gmail.com>
2016-02-04 22:58:55 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!(self =~ /\.$/)    
   end
 
   def question?
-
+    !!(self =~ /\?$/)
   end
 
   def exclamation?
-
+    !!(self =~ /!/)
   end
 
   def count_sentences
-
+    self.split(/\.+|!+|\?/).count
   end
 end
