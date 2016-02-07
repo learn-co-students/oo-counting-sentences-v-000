8b74a85925619273bf5b982c60faf43daae0862c
smithWEBtek <brad@smithwebtek.com>
2016-02-07 13:52:18 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,13 +21,12 @@ class String
   end
 
   def count_sentences
-    # temp1 = ""
-    # matches = ""
-    # match_count = 0
-    # temp1 = self.split(". ")
-    # matches =  
-    self.scan(/([A-Z][\w\s\d,]+[\.!?]+)/).size
-    # match_count = matches.length
+    temp1 = ""
+    matches = ""
+    match_count = 0
+    temp1 = self.split(". ")
+    matches =  self.scan(/([A-Z][\w\s\d,]+[\.!?]+)/)
+    match_count = matches.length
 
 # puts "---------------OUPUT REPORT---(UNCOMMENT TO DISPLAY)--------------"
 # puts "Self (untouched): #{self}"
@@ -36,10 +35,10 @@ class String
 # puts "matches, regex matches: #{matches}"
 # puts "match_count: #{match_count}"
     
-    # if match_count == 0; return temp1.length
-    # elsif match_count > temp1.length; return match_count
-    # else 
-    # end
+    if match_count == 0; return temp1.length
+    elsif match_count > temp1.length; return match_count
+    else 
+    end
   end
 end
 
