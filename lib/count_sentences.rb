eae9ea5fd0bafd927920bf6ffc6f64b680851cc2
gnfisher <me@gnfisher.com>
2016-03-09 11:21:48 -0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/[.]$/)
   end
 
   def question?
-
+    !!self.match(/[?]$/)
   end
 
   def exclamation?
-
+    !!self.match(/[!]$/)
   end
 
   def count_sentences
-
+    self.split(/[.!?]+/).count
   end
 end
