d545b057f56ce997e3ad9a1bf8d9355a387ff54f
DownsideupmediaUSA <brandondecarlo@live.com>
2016-04-01 09:30:23 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,31 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    if self.to_s.end_with? (".")
+      true
+    else 
+    false
+  end
   end
 
   def question?
-
+    if self.to_s.end_with? ("?")
+          true
+        else 
+        false
+    end
   end
 
   def exclamation?
-
+    if self.to_s.end_with? ("!")
+          true
+        else 
+        false
+    end
   end
 
   def count_sentences
-
+     self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
