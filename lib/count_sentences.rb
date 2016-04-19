2edd2bc6a7fd3838566ed4329a52b971c47625c1
zszalas2 <zszalas@gmail.com>
2016-04-19 12:23:43 -0400

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #if sentence ends in period = true
+    #if sentence ends in anything else return false
+    self.end_with?(".")? true : false 
   end
 
   def question?
-
+    self.end_with?("?")? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
+  
+    sentence_array = self.split(/[.?!|...|!!]/)
+    sentence_array.delete("")
+    return sentence_array.length
 
   end
 end
