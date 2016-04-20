2147fe40263f1aff5bfd34def7ea419671cd1bbc
Saul Ocampo <digitalh2o@gmail.com>
2016-04-20 01:23:02 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   @@words = []
+    self.split(/[?!.]/).map do |word|
+      if word == ""
+        word.delete
+      else
+      	@@words << word
+      end
+    end
+    @@words.count
+    #binding.pry
   end
+
 end
