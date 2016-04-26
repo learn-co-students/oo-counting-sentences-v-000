0c914c24c7d148e58866e86f1a69cc8ef42338df
andytouch93 <andy@xavierdesignstudios.com>
2016-04-25 19:00:32 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentences = []
+    self.split(/[?!.]/).each do |sentence|
+      if sentence != "" 
+        sentences << sentence
+      end
+    end
+    sentences.count
   end
 end
