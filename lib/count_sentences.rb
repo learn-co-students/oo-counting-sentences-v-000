62bf2e8656ae1e21733173ac73ab2070faf9f6fc
Being Young <beingy@gmail.com>
2016-05-13 19:13:38 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,9 @@ class String
   end
 
   def count_sentences
-    self.scan(/(\?|\.|\!)+(\s|$)/).length #=> http://rubular.com/r/3Ca2BYu0gz
+    # self.scan(/(\?|\.|\!)+(\s|$)/).length #=> http://rubular.com/r/3Ca2BYu0gz
+    self.split(/\?|\.|\!/).delete_if { |letters| letters.length < 2 }.length
   end
 end
+
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
