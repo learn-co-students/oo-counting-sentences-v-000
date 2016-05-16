2b377c15f563b1ac923578fac466b79f18db6266
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-16 13:20:12 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #self.end_with?(".")
+    self[-1]=="."? true : false
   end
 
   def question?
-
+    self[-1]=="?"? true:false
   end
 
   def exclamation?
-
+    self[-1]=="!"? true:false
   end
 
   def count_sentences
-
+    return self.split(/\.|\?|\!/).delete_if {|var| var.size < 2}.size
   end
 end
