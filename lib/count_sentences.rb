eaf8af229ca16bd479f1a3dae1960bf91e6f4140
Eric Chu <erickentleongchu@gmail.com>
2016-05-17 12:43:44 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sent_count = 0
+    self.split("!?.")
 
+    self.each_with_index do |x, i|
+      if x == ""
+        self.delete_at(i)
+      else sent_count += 1
+      end
+    end
+    sent_count
   end
 end
