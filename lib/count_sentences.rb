e5481e37cba8ec011a3ad9ecb23b09be484000d7
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-24 22:43:45 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    proto_sentences = self.split(/[.?!]/)
+    sentences = proto_sentences.select do |sentence|
+      sentence != ""
+    end
+    sentences.length
   end
 end
