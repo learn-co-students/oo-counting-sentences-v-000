21f479f870fc07f391759acea5bec077315f9f58
Sandy Hogan <svern99@aol.com>
2016-06-02 16:52:49 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentence_split = self.split(/\w+[\.|\!|\?]+/)
+    counted_sentences = sentence_split.count
+    return counted_sentences
   end
 end
