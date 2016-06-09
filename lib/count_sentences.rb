e5bc13ba5251faaf5f4acb40a2f47cebc8c45aff
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-09 20:28:32 +0000

    initial commmit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self
+      if string.include?(".")
+      	 num_of_sentences = string.count(".")   
+         num_of_sentences                                               
+      else
+         0
+      end
   end
+#
 end
