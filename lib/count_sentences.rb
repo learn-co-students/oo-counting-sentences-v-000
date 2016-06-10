6c2a45f1701807b8d87aad0df83adc272425daeb
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-10 01:20:15 +0000

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,12 +17,17 @@ class String
 
   def count_sentences
     self
-      if string.include?(".")
-      	 num_of_sentences = string.count(".")   
-         num_of_sentences                                               
-      else
-         0
-      end
+    if self.end_with?(".") || self.end_with?("!") || self.end_with?("?") #check for all the edge cases
+       new_string=[]
+       #split the string at every sentence delimiter to singel them out later
+       new_string = self.split(/[.?!]/).delete_if do | string_item | #regexp and .split to match specific delimiters
+       #don't include any empty spaces that are formed from a double !! etc by deleting empty spaces
+       string_item ==""
+       end
+       new_string.count #counts the strings in the array without the extra space that was deleted
+    else
+      0
+    end
   end
-#
+
 end
