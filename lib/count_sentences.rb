48900b658b15f1d52debd9927d9bd927ed1c86f3
Jesse Novotny <jessenovotny@gmail.com>
2016-06-20 16:36:34 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.scan(/\W$/) == [] ? false : true
   end
 
   def question?
-
+    self.scan(/[?]$/) == [] ? false : true
   end
 
   def exclamation?
-
+    self.scan(/[!]$/) == [] ? false : true
   end
 
   def count_sentences
-
+    sentence = []
+    sentence = self.scan(/(\w[.?!])/)
+    sentence.length
   end
 end
