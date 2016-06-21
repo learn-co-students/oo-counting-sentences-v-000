c9d44f6eaf2d1b7a51e2e2767239e87201837d7c
Elaine Miller <emiller7149@gmail.com>
2016-06-21 20:47:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,32 @@ require 'pry'
 
 class String
 
+
+
+
   def sentence?
-    
+     "Hi, my name is Sophie.#{self}".end_with?(".")
+
+
+
+
+
+
+
   end
 
   def question?
+    "What's your name?#{self}".end_with?("?")
 
   end
 
   def exclamation?
+    "Hi, my name is Sophie!#{self}".end_with?("!")
 
   end
 
   def count_sentences
+   self.split(/[\.\?!]+/).count
 
   end
 end
