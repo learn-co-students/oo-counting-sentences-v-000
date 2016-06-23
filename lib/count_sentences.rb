339dae84cde33fd698fb9f786e36f6aa7c844a92
yeungn <nin.yeung@gmail.com>
2016-06-23 13:44:44 -0400

    refactoring counting_sentence.rb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,29 +1,17 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    if self.end_with?(".")
-      true
-    else
-      false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
