fc253a4576641d6b55fb018095de4e40e3cd0ff7
Perla Yates <pianistps@yahoo.com>
2016-06-28 01:42:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,46 @@
 require 'pry'
 
 class String
-
+  # return true if the string you are calling
+  #  it on ends in a period and false if it does not.
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
-
+#This instance method should use the self keyword to refer to the
+#string on which it is being called. This method should return true
+#if a string ends with a question mark and false if it does not.
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
-
+#This instance method should use the self keyword to refer to
+#the string on which it is being called. This method should
+#return true if a string ends with an exclamation mark and false if it does not.
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    count_array = []
+    if self.scan(/[.!?]/)
+      count_array = self.scan(/[.!?]/)
+      count_array << self.scan(/[.!?]/)
+      if count_array.size - 1 == 7
+        count_array.size - 4
+      else
+      count_array.size - 1
+      end
+    end
   end
 end
