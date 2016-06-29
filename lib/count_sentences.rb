ce1deee996719ed46745743def00c492170a2721
andrewmmarquis <andrewmmarquis@gmail.com>
2016-06-29 16:18:43 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = []
+    split_sentence << self.split(".", "!", "!!", "?")
+    if split_sentence.count == 0
+      0
+    else 
+      split_sentence[].count
+    end
   end
 end
