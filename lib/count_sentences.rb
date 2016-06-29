9883978f1cd4bd6d8dd0573ae7a6f422c5638e1e
peterberwind <peterberwind@gmail.com>
2016-06-28 23:58:39 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if !self.end_with?(".")
+      return false
+    else
+      return true
+    end
   end
 
   def question?
-
+    if !self.end_with?("?")
+      return false
+    else
+      return true
+    end
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
+
 end
