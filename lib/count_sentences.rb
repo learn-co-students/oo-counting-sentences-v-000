4bbcd5e1ef5c865b7d325934adb1bd13bf1cb921
Certain125 <amanita.certain@gmail.com>
2016-06-29 13:11:53 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|x| x.size < 2}.size
   end
 end
+
+#The practice of adding methods to or altering Ruby's built in classes is called monkey patching.
