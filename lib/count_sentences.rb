9b079159e35bd16ddf44e03c6af5c7b4791f07c9
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-03 17:57:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    @count = 0
+    if self.question?
+      @count += 1
+    end
 
+    if self.sentence?
+      @count += 1
+    end
+
+    if self.exclamation?
+      @count += 1
+    end
+
+    @count += self.split(".").compact.size - 1
+    @count += self.split("?").compact.size - 1
+    @count += self.split("!!").compact.size - 1
+    if @count < 0
+      @count = 0
+    end
+    return @count
   end
 end
