88af82940da766809844b2024eaee9f59332587a
Emmanuel Owusu <e_owusu@live.com>
2016-08-05 02:30:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #new_array = self.split('.').reject{|s| s.empty? || s == " "}
+    #return new_array.length
+    string = self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+    return string
   end
 end
