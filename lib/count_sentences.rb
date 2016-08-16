61863ed2761307e84c52d6795786445e6ff95164
John Schlesinger <jschles1@ramapo.edu>
2016-08-16 01:09:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    string = self.split(/[.!?]/)
+    string.delete("")
+    string.count
   end
 end
