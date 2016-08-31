7dd3aba7bafdf5f09546102273d2eadd0a1ed1c4
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-08-31 02:01:42 +0000

    count sentences instance method

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence = self.split(/[.?!]/)
+    counter = 0
+    sentence.each do |element|
+        if element.include?("#{/[.?!]/}")
+        elsif element.length > 1
+          counter += 1
+        end
+    end
+    counter
   end
 end
