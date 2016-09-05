b4c9b742a7db2b940ab03251df7f343e8ca51199
Pamela Bortnick <pbortnick@gmail.com>
2016-09-05 17:51:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count_split = self.split(/[.?!]/)
+    binding.pry
+    count_split.count
   end
 end
