1ff00b4abad5556dd56c884c1fb83ad86c519157
smithWEBtek <brad@smithwebtek.com>
2016-09-05 19:20:24 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,20 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?("."); return true
-    else return false
+    if self.end_with?("."); true
+    else false
     end
   end
 
   def question?
-    if self.end_with?("?"); return true
-    else return false
+    if self.end_with?("?"); true
+    else false
     end
   end
 
   def exclamation?
-    if self.end_with?("!"); return true
-    else return false
+    if self.end_with?("!"); true
+    else false
     end
   end
 
@@ -26,21 +26,13 @@ class String
     match_count = 0
     temp1 = self.split(". ")
     matches =  self.scan(/([A-Z][\w\s\d,]+[\.!?]+)/)
-    match_count = matches.length
-
-# puts "---------------OUPUT REPORT---(UNCOMMENT TO DISPLAY)--------------"
-# puts "Self (untouched): #{self}"
-# puts "temp1, split on_._:     #{temp1}"
-# puts "temp1.length:            #{temp1.length}"
-# puts "matches, regex matches: #{matches}"
-# puts "match_count: #{match_count}"
+    match_count = matches.length 
     
-    if match_count == 0; return temp1.length
-    elsif match_count > temp1.length; return match_count
+    if match_count == 0
+      temp1.length
+    elsif match_count > temp1.length
+      match_count
     else 
     end
   end
 end
-
- 
-
