f4932f3ab8555ba43133dfff4b02d7829fcfeb22
Juan Gongora <gongora.animations@gmail.com>
2016-10-05 02:49:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[^a-z,A-Z\s]\W*/).count
   end
 end
