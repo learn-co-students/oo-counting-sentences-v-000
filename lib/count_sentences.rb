3519712574d74aa88781a4e90eb0cb6e632c34bf
Ray Mulligan <rmullig2@optonline.net>
2016-10-13 00:59:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/.*\.\Z/) ? true : false
   end
 
   def question?
-
+    self.match(/.*\?\Z/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/.*\!\Z/) ? true : false
   end
 
   def count_sentences
-
+    self.scan(/.*?[\.\?\!]+/).length
   end
 end
