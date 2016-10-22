aa24dfeea1a5e245e856cd77b6641e1c8fc7c0bf
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2016-10-22 21:33:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/\.$/)
   end
 
   def question?
-
+      !!self.match(/\?$/)
   end
 
   def exclamation?
-
+      !!self.match(/\!$/)
   end
 
   def count_sentences
-
+      x=self.split.select {|item| item.match(/[\!\?\.]$/)}
+      x.length
   end
 end
