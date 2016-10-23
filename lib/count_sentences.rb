f2f9d2fcbd9a8784ec4a46bf76846a35f14de55a
Ryan Evans <ryanwevans@icloud.com>
2016-10-23 00:27:00 -0700

    add string instance methods

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\!|\?/)
+    sentences.delete_if {|e| e.length == 0}
+    sentences.count
   end
+  # binding.pry
 end
