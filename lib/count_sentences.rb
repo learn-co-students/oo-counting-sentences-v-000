c36d50baa168389aaa7b740cdd525fde7f75f76e
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-28 09:23:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1]=='.'
   end
 
   def question?
-
+    self[self.length-1]=='?'
   end
 
   def exclamation?
-
+    self[self.length-1]=='!'
   end
 
   def count_sentences
-
+     count=0
+     for i in 1..(self.length-1)
+       if (self[i]=='.'||self[i]=='?'||self[i]=='!')&&
+         !(self[i-1]=='.'||self[i-1]=='?'||self[i-1]=='!')
+         count+=1
+       end
+     end
+     count
   end
 end
