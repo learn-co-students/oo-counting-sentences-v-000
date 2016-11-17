fa0518833d7055ce53f03d5c341e1458f2efd4ce
Elias Ventura <eliasventura23@gmail.com>
2016-11-17 20:14:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,6 +1,5 @@
-'require pry'
-
 class String
+
   def sentence?
     if self.end_with?(".")
       true
@@ -10,22 +9,14 @@ class String
   end
 
   def question?
-    if self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?") ? true:false
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+    self.end_with?("!") ? true:false
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if {|w|w.size < 2}.size
   end
 end
