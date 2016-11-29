61b281882b037e11b34a2dc3342a4a5d9517fab0
Daniel Warren <warrend555@gmail.com>
2016-11-29 00:44:53 -0500

    Finishes oo-counting

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    regex = /\.|\?|\!/
+    self.split(regex).delete_if { |i| i.length < 2 }.count
   end
 end
