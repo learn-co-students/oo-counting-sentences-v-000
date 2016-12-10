a80b6f7aa988d7201461245fb85745474ca77158
Yamini Kumawat <yaminikumawat94@gmail.com>
2016-12-10 03:21:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = []
+    if self.include?(".") || self.include?("!") || self.include?("?")
+      array = self.split(/[.!?]+/)
+      return array.count
+    else
+      return 0
+    end
   end
 end
