e84c2f36bfa3d45e49120b35af0579cc5c7f0131
Alyssa Sidebottom <alyssa.m.sidebottom@gmail.com>
2016-12-27 19:26:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_array = self.scan(/[a-zA-Z\s,]+[^.?!]/)
+    return split_array.length
   end
 end
