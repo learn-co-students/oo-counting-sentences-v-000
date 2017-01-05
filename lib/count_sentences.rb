bab55ef5de785b7fede46dc9b040a33e525d218a
ryanpmcmahon <ryan@mcmahon.work>
2017-01-04 20:38:59 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/.+[.]/) ? true : false
   end
 
   def question?
-
+    self.match(/.+[?]/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/.+[!]/) ? true : false
   end
 
   def count_sentences
-
+    arr = self.split(/[!.?]/)
+    arr.count{|x| x.length > 0}
   end
 end
