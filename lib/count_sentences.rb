6ecdf8ec333b9ee0cc0bd0993aa16b451399341e
Damien de Lartigue <damien@delartigue.co.uk>
2017-01-31 16:11:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,13 +16,7 @@ class String
 
   def count_sentences
     str = self.split(/[.?!]/)
-    #binding.pry
     str.reject! {|i| i.empty?}
-    #binding.pry
     str.count
   end
 end
-
-complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-
-complex_string.count_sentences
