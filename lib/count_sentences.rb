5b153e2f4bd4430c7a273025c0edf92804a3d999
Erika Hughes <ehughes@knights.ucf.edu>
2017-02-15 19:50:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.each do |sentence|
+      if sentence == ""
+        sentences.delete(sentence)
+      end
+    end
+    count = sentences.count
+    # binding.pry
+    count
   end
+
 end
