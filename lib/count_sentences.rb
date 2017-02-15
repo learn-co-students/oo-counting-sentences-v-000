ee5b6d10f0db2dc454ede683bb70fbd1ca7c1263
Omar Mason <mr.omarmason@gmail.com>
2017-02-15 01:15:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true :  false
+      
   end
 
   def question?
-
+    self.end_with?("?") ? true :  false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true :  false
   end
 
   def count_sentences
-
+    punctuation = '.?!'
+    count = 0 
+    self.split(" ").each { |word| count += 1 if punctuation.include?(word[-1]) }
+    count
   end
+  
 end
