9771d0454153f0abf5541aa154bd407cb0ece43b
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-02-19 05:05:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    bool = self.end_with?(".")
+    bool
   end
 
   def question?
-
+    bool = self.end_with?("?")
+    bool
   end
 
   def exclamation?
-
+    bool = self.end_with?("!")
+    bool
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.each do |e|
+      if e.length == 0
+        sentences.delete(e)
+      end 
+    end
+    sentences.count
   end
 end
