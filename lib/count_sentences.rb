008c94098ef2c2d2e7307eff354853e33c027283
Nicholas Frank <nicholas.edmunds.frank@gmail.com>
2017-03-02 02:57:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array_w_error = self.split(/[!.?]/)
+    array_w_error.delete("")
+    array_w_error.length
   end
 end
