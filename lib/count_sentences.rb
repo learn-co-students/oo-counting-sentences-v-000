e0e2172394f8ba590a4221a1963f26fa3afe3e9e
Michael Etlinger <mk.etlinger@gmail.com>
2017-03-09 17:17:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentence, :question, :exclamation, :count_sentences
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!.?]+/).count
   end
+
 end
