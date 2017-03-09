a97aa2f3caaef2ca5f20e00dab591e137fc90b6d
Hima Chitalia <hima_chhag@yahoo.com>
2017-03-09 21:42:06 +0000

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|\!/).delete_if {|n| n.empty? }.length
   end
 end
