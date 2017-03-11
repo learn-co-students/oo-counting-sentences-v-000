2fafe4f05eabe96495732a2415f67391990a5d9d
Rachel Walwood <walwoodr@gmail.com>
2017-03-11 16:42:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    /[.]\z/ === self
   end
 
   def question?
-
+    /[?]\z/ === self
   end
 
   def exclamation?
-
+    /[!]\z/ === self
   end
 
   def count_sentences
-
+    self.split(/[.!?] /).size
   end
 end
