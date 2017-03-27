3cbcc9843ad0ad4185a11057c1e2e59f7a4d48f6
antley chu <antley.chu@gmail.com>
2017-03-27 03:10:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+        true
+      else
+        false
+      end
+
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = []
+    sentences = self.split(/[!?.]/)
+    string = sentences.collect do |string|
+      if string == ""
+        sentences.delete(string)
+      else
+        string
+      end
+    end
+    string.count
   end
 end
