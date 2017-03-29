1b99948e4562408507968b4f1a4d1335a2f858eb
Lucas Kisabeth <lukey.mac@outlook.com>
2017-03-29 16:45:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    split_sentences = nil
+    split_sentences = self.split(/\.|\?|\!/)
+    split_sentences.delete_if {|sentence| sentence.size < 2}
+    split_sentences.size
   end
 end
