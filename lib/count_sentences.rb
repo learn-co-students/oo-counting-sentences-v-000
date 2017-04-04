73c23461e536b108b388184502516d39256b56ec
Michael Ries <michael.ries@gmail.com>
2017-04-04 16:29:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1]=='.'?true:false
   end
 
   def question?
-
+    self[-1]=='?'?true:false
   end
 
   def exclamation?
-
+    self[-1]=='!'?true:false
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences.select! {|sentence| sentence.length>0 }
+    sentences.size
   end
 end
