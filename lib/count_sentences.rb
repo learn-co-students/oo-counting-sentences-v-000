7dc5159d9a6a580f09be4c812e1e67d37f30ddfc
Julie Widman <juliewdmn@gmail.com>
2017-04-11 19:36:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    #binding.pry
+    self.split(/[.!?]/).reject{|e| e.to_s.empty?}.count
 
   end
 end
+
+#sentence?.question?.exclamation?.
+#arr.reject { |e| e.to_s.empty? } #=> [1, 2, "s", "d"]
+
+#arr.reject { |item| item.blank? }
+
+#arr = arr.compact.reject { |h| h == "" }
+
+#arr = arr.compact.delete_if { |h| h == "" }
+#self.split(/[\!\s|\.\s|\?\s]$/).count.reject{|e| e.empty?}
