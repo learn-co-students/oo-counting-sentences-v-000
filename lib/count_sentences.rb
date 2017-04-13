deb0db77a4100b550a9262d2e95ccf364e02b34c
David Mars <david.w.mars@my.occc.edu>
2017-04-13 05:27:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
+#   ~~~HAVICK WAS HERE~~~
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
-  def count_sentences
+
+def count_sentences
+  	self.split(/[?.!]/).delete_if{|x|x == ""}.count
 
   end
 end
