9da590872ae08372ba3cd77e6c330cd7abb6001b
Saad <Dropheaven@live.com>
2017-04-14 21:54:47 -0600

    Lab complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    counter = 0
+    array = self.split("")
 
+    array.each_with_index do |char, index|
+      if (char == "." || char == "?" || char == "!") && (array[index - 1] != char)
+        counter += 1
+      end
+    end
+    counter
   end
 end
