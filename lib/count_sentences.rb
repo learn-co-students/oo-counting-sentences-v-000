e432cbc63e282888a56634583bc361c1cc4d6528
Caitlin Lamborne <malfene@netscape.net>
2017-04-27 18:58:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_count = self.split(/[.!?]\s/).count
+      new_count
   end
 end
