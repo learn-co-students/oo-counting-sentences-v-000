26f5f1fa62a92245c7fad40fba60907f1cc0e989
Noah Summers <summers.noah@gmail.com>
2017-05-18 19:06:17 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    match(/\u{002e}\z/) ? true : false
   end
 
   def question?
-
+    match(/\u{003f}\z/) ? true : false
   end
 
   def exclamation?
-
+    match(/\u{0021}\z/) ? true : false
   end
 
   def count_sentences
-
+    scan(/\b[\u{002e}\u{003f}\u{0021}]+/).length
   end
 end
