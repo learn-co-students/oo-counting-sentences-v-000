6a07214ac910ef782e83dda4f48727b79ebb4608
Sullivan Nolan <nolakat@gmail.com>
2017-05-20 11:15:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    i = 0
+     self.split.each do |var| 
+      if var[-1].include?('.') || var[-1].include?('!') || var[-1].include?('?')
+        i +=1
+      end 
+     end 
+     return i
 end
+end 
