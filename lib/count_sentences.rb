5a8c6a93323ca766b850f282aea517d3015e3cc0
Kristy Peterson <kpeterson.kap@gmail.com>
2017-05-22 17:39:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+      self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+      self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    array = []
+    array = self.split(/[\.\?\!]/)
+    array.delete_if {|sentence| sentence == ""}
+    #if array.include?(" ")
+    #  array.delete sentence}
+  #binding.pry
+    array.size
   end
 end
