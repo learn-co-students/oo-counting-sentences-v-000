3a4f62dce7d87486a86cb1460e5354b0cd8b768c
Derek  Pelt <derekpelt@gmail.com>
2017-05-24 07:05:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,26 +7,16 @@ class String
   end
 
   def question?
-    if self.end_with?("?")
-      true 
-    else
-      false 
-    end 
+    self.end_with?("?") 
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else 
-      false 
-    end 
+    self.end_with?("!")
   end
 
   def count_sentences
     new = []
     new = self.split(/[!.?]/)
     new.reject {|i| i.nil? || i == ""}.count
-    #new.count 
-    #binding.pry 
   end
 end
