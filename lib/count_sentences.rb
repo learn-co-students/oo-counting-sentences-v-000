aa93b8cf48b725fa61d5e95107808fe37b8c2f47
ctab33 <ctab33@me.com>
2017-05-25 19:11:00 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #if self.scan(/\.|\?|\!/).count == 0
+      #return 0
+    #else
+      #
+    #binding.pry
+    #end
+    #self.scan(/\.|\?|\!/).count
+    scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
