5a1f7f6ee6c51fb8e1cfdd80a6a0f4e1bfa44a4a
Richard Burd <richardburdx@yahoo.com>
2017-05-25 22:30:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
-
+#   This works on repl.it but not on Learn IDE for some reason.
+#   self.split(/\.|\?|\!/).size
+#   This works on repl.it too...so I dunno what the hell is going on.
+#   self.split(/\.|\?|\!/) {|i|}.size
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
