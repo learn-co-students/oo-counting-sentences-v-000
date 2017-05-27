c0e57130230081431b6a10284f9851f306e08d4d
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-05-27 20:17:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    question? || exclamation? || self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = 0
+    found_flag = false
+    self.each_char do |character|
+      if character == "." || character == "?" || character == "!"
+        if !found_flag
+           sentences += 1
+           found_flag = true
+        end
+      else
+        found_flag = false
+      end
+    end
+    sentences
   end
 end
