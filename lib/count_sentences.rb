c7c149037d8e06294e744f25e80326f135829ded
Emily Howard <howard.emilya@gmail.com>
2017-06-03 14:26:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = []
+    sentences = self.split(/\.|\!|\?/)
+    sentences.delete_if do |things|
+      things == ""
+    end
+    sentences.count
   end
 end
