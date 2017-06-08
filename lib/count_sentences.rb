7a936b1f9ddba2725043bd74604281b787d401df
Tyler Gotto <tylergotto17@gmail.com>
2017-06-08 08:19:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|\!/)
+    sentences.collect do |x|
+        if x.size < 2
+            sentences.delete(x)
+        end
+    end
+    sentences.size
   end
 end
