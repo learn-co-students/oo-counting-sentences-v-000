a30aa2aa1650febcc7dbac7faec6b56e9b3df4df
Yujun Sung <yujunsung@gmail.com>
2017-06-14 00:01:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
 
+    counter = self.split(/[.!?]\s/).count
+
+    if counter == 0
+      0
+    else
+      counter
+    end
   end
 end
