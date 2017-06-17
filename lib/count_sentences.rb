56f8a321819450f88cb4f0aaa5e41af0215e8765
Christina <github email address>
2017-06-16 19:26:44 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
+  attr_accessor :sentence, :question, :exclamation
 
   def sentence?
-    
+    self.end_with?(".") == true
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if { |word| word.size < 2}.size
   end
 end
