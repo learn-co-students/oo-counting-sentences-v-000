59825cf33e83deb5576efc8a2aac4a83dd937349
Sydney Chun <schun08@gmail.com>
2017-06-20 18:12:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #need to split the string based on multiple delimiters: ?, ., !
+    #can you use a regex to return the matches
+      #regex - ends with /[.!?]/
+    arr = self.split(/[.!?]/)
+    arr.delete_if {|item| item == ""} #delete empty items
+    arr.length
   end
 end
