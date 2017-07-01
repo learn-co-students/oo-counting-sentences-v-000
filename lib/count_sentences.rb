69668aaf48a5662dc795d3d6715c8e84cf2c3363
Sara Stanton <sara.mpa13@gmail.com>
2017-07-01 08:56:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else return false
+    end
+    self.sentence?
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else return false
+    end
+    self.question?
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else return false
+    end
+    self.exclamation?
   end
 
   def count_sentences
-
+    sentences = []
+    self.split(/\.|\?|\!/).each do |x|
+      if x != ""
+        sentences << x
+      end
+    end
+    sentences.length
   end
 end
