5da7f562130f6f10b95da0bf989befcb7a818abb
Sam Crowder <samecrowder@gmail.com>
2017-07-03 20:51:46 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+  	self.end_with?('?')
   end
 
   def exclamation?
-
+  	self.end_with?('!')
   end
 
   def count_sentences
 
-  end
+  #split_by_period = self.split('.')
+  #split_by_comma = []
+  #split
+
+  #split_by_period.each do |splitting|
+  	#split_by_comma << splitting.split(',')
+  #end
+
+  #split
+
+  self.split('.',',','!','?')
 end
