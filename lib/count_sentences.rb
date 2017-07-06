621983afe980e4576c09333ded58e73840acfe40
Sarah Schaller <sarcsch@aol.com>
2017-07-06 12:58:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") != false
   end
 
   def question?
-
+    self.end_with?("?") != false
   end
 
   def exclamation?
-
+    self.end_with?("!") != false
   end
 
   def count_sentences
-
+    self.split.count {|element| element.sentence? || element.question? || element.exclamation?}
+    #if self.sentence? === true || self.question? === true || self.exclamation? === true
+    #  self.split.count
   end
 end
