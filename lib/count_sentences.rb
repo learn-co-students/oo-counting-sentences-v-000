4becebee432d58e1389c8b702a27bbd532269d1e
Femi F <ofpupp@princeton.edu>
2017-07-14 16:11:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    counter = 0
+    count = self.split(/[.?!]/)
+    count.each do |x|
+      if x != ""
+        counter = counter+1
+      end
+    end
+    return counter
+    end
 end
