dca9a415f2fbdd5e857ef806fe00b44ae45e9b70
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-18 01:04:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
+    c=0
+    count=0
+  while (c <self.length)
 
-  end
+    if (self[c]=="!")
+         if (self[c+1]=="!")
+         else
+            count+=1
+         end
+       end
+
+       if (self[c]==".")
+            if (self[c+1]==".")
+            else
+               count+=1
+            end
+          end
+
+          if (self[c]=="?")
+               if (self[c+1]=="?")
+               else
+                  count+=1
+               end
+             end
+
+     c+=1
+   end
+ count
+end
 end
