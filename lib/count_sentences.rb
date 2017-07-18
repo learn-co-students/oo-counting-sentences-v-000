a531268553ea97d59508824bd22c96f0129cca4f
James Randolph <james.randolph2016@gmail.com>
2017-07-18 00:29:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    testing = self.split(/[.?!]/)
+    testing.delete_if do |string|
+      string.empty?
+    end
+    testing.size
   end
+
 end
