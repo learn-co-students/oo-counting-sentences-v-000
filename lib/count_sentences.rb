d5385242c12ff705e75500f0ac91e53fd6e018ae
lita stephenson <lita.stephenson@gmail.com>
2017-08-31 22:52:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    self.ends_with?(".")
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[!.?]/)
+    sentences.reject! {|el| el == "" || el == " "}
+    return sentences.length
   end
+
 end
