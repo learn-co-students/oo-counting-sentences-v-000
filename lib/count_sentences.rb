23dfd47f908a50eb7795039505da08656fe8451b
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-07 17:41:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
+  # def end_with?(character)
+  #   if character == self[-1]
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split(/[?!.]/)
+    if new_array.length == 0
+      return 0
+    else
+      if new_array.include?("")
+        new_array.delete("")
+        new_array.length
+      else
+        new_array.length
+      end
+    end
   end
+
 end
