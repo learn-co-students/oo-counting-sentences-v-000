851c524ae3fc89960f01dfd906bc50e467b6c15d
aisayo <aisayo@icloud.com>
2017-09-08 19:08:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
-require 'pry'
-
 class String
 
-  def sentence?
-    
-  end
+    def sentence?
+      if self.end_with?(".")
+        return true
+      else
+        false
+      end
+    end
 
   def question?
-
-  end
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    self.squeeze('.!?').split(/[?.!]/).length
 
   end
 end
