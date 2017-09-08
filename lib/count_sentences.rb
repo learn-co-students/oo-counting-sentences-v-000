8f127edb488e944f2b317ac5cacadc67383b3bd4
Sally Zasloff <szasloff@gmail.com>
2017-09-08 18:02:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if{|element| element==""}.size
+    self.split(/\.|\?|\!/).delete_if{|element| element==""}.count
   end
 end
