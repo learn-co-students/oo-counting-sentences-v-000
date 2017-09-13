8c105188c90063f246699459879c9441411b2ed5
Melissa Amaya <amaya.melissa@gmail.com>
2017-09-13 03:21:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")? true : false
   end
 
   def question?
-
+    self.end_with?("?")? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
+   temp = self.split(/\b | \s/)
+   binding.pry
+
+
 
   end
 end
