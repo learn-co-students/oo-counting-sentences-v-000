b6c59e662844ea9fba893bad0e4253302595d1a2
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-13 16:58:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
+
 
   def question?
-
+    self.end_with?("?")
   end
+
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
 
   def count_sentences
-
+    count = self.split(".")
+    count.each do |i|
+      placeholder = (i.split("?"))
+      if placeholder.size != 1
+        count.push(placeholder[0])
+        count.push(placeholder[1])
+      end
+    end
+    count.each_with_index do |i, index|
+      if i == ""
+        count.delete_at(index)
+      end
+    end
+    count.size
   end
+
 end
