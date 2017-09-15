355bfca43dbd268f28dfeb347254582c01b1a8cb
Alyssa Ragusa <alyssa.ragusa07@yahoo.com>
2017-09-15 14:12:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    return false if !(self.end_with?("."))
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    return false if !(self.end_with?("?"))
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    return false if !(self.end_with?("!"))
   end
 
   def count_sentences
-
+    count = self.split(/\.|\?|\!!/).length
+    count
   end
 end
