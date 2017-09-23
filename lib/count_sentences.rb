e3f6075222d6c80f473fa8a979b517c3890b7bba
ff18qd <ff18qd@gmail.com>
2017-09-23 00:42:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counts = []
+    counts=self.split(/\!|\.|\?/)
+    num = counts.size
+    counts.each do |it|
+      if it.length == 0
+        num -=1
+      end
+    end
+    num
   end
 end
