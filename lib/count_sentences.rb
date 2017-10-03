831a4bf2972c995223c6168050296d3951a7857d
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-03 11:41:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array=self.split(/[.?!]/)
+    new_array.delete_if{|potential_sentence| potential_sentence == ""}
+    new_array.length
   end
 end
