6e7236cbe453253594fbf11bf201d1d20e6cec70
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-09 17:50:51 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+  if self.end_with?(".")
+    true
+  else
+    false
+  end
   end
 
   def question?
-
+if self.end_with?("?")
+  true
+else
+  false
+  end
   end
 
   def exclamation?
-
+if self.end_with?("!")
+  true
+else
+  false
+  end
   end
 
   def count_sentences
-
+    array = []
+    string = self.split('')
+    string.each_with_index do |x, y|
+      if x == "." || x == "?" || x == "!"
+        array << x unless string[y-1] == x
   end
+  end
+  array.length
+end
 end
