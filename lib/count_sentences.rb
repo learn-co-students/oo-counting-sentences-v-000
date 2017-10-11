06fb3d424309221df36a9cd39c13803ffe33e0b0
hailey endres <hailey.endres@gmail.com>
2017-10-11 22:52:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    array = self.split(/[\.!?]/)
 
+    array.each do |sentence|
+      if sentence == ""
+        array.delete(sentence)
+      end
+    end
+
+    array.count
   end
 end
