35a79ffc9f3830353f0a50ea2086805d8a5d72f7
kristeneknorn <kristen.knorn@gmail.com>
2017-10-13 18:56:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,31 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+def sentence?
+    if self.match(/[\.]\z/)
+       return true
+    else
+      return false
+end
+end
+
+def question?
+  if self.match(/[\?]\z/)
+       return true
+  else
+      return false
+end
+end
+
+def exclamation?
+  if self.match(/[\!]\z/)
+         return true
+  else
+        return false
   end
-
-  def question?
-
-  end
-
-  def exclamation?
-
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
