8150b136ed71a8b14aa1699f1fd6b5f448d8e615
Kevin Huang <yellow.khaos@gmail.com>
2017-10-22 23:57:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+    final_array = []
+    array.each do |item|
+      final_array<<item if item!=""
+    end
+    final_array.length
   end
 end
