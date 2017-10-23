21ff592008382470928409015f2243808b171985
Christopher DiStasio <cadistasio@gmail.com>
2017-10-23 22:22:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.ends_with?(". ")
+      self.split(". ")
+      # if self.ends_with?(". ")
+      #   self.split(". ")
   end
 end
+
+binding.pry
