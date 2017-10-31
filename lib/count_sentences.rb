62dbe3ffc80613e1fd02abc8845ebb10797633e7
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-31 17:58:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.split("").last == "."
+      return true
+    end
+    false
   end
 
   def question?
-
+    if self.split("").last == "?"
+      return true
+    end
+    false
   end
 
   def exclamation?
-
+    if self.split("").last == "!"
+      return true
+    end
+    false
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).count
   end
 end
