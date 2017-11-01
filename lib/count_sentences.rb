ab8725ac13071bfe18bb2b9f1e437e170881eb56
Nolan Hughes <nhughes987@gmail.com>
2017-11-01 20:30:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject do |e|
+      e.to_s.empty?
+    end.count
   end
+
 end
