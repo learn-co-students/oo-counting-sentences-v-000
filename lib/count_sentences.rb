5737f03a404821df64132324bd1b15ea38a0036b
Rebecca Tong <rtong@buffalo.edu>
2017-11-05 02:29:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+end
+return false
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+  end
+  return false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    end
+    return false
   end
 
   def count_sentences
-
-  end
+    values = split(/\.|\?|\!\s/)
+    values.count
+    end
 end
