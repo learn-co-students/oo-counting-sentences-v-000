dfe9a1037dbf43eb55c40344d8ae6d78e927e13e
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-12 12:11:52 -0500

    monkeypatched String

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if "." == self[-1]
+      return true
+    end
+
+    false
   end
 
   def question?
+    if "?" == self[-1]
+      return true
+    end
 
+    false
   end
 
   def exclamation?
+    if "!" == self[-1]
+      return true
+    end
 
+    false
   end
 
   def count_sentences
-
+    self.split(/[\.\?!] /).length
   end
 end
