945636a82864b158856987193c621a9d919abd4f
Anthony Harvey <anthonygharvey@gmail.com>
2017-11-29 22:43:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+        x = 0
+        self.split(/[.!?]/).each do |element|
+            if element != ""
+                # binding.pry
+                x += 1
+            end
+        end
+        x
+    end
 
-  end
+
 end
