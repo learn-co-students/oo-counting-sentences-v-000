0b820f0468ed8c83fcae427404c26ba567273a31
Yina Zhu <yinagit@gmail.com>
2017-11-29 04:47:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).delete_if {|word| word == ""}.count
+    # took the string, split it by its punctuation, then i deleted it if the word
+    # equals an empty string "", then i counted the elements
   end
 end
