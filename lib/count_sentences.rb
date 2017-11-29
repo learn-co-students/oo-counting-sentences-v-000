f50b029287a8101f6b461c9a571fc51f843d9313
John Wojciechowski <jwojo13@gmail.com>
2017-11-29 20:25:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    words = []
+    not_words = []
+    if !self.include?("." || "?" || "!")
+      0
+    else
+      self.include?(".")
+      self.split(/(\?|\.|\!)/).each do |target|
+        if target == "." || target == "!" || target == "?" || target == ""
+          not_words << target
+        else
+          words << target
+        end
+      end
+      words.count
+    end
   end
 end
