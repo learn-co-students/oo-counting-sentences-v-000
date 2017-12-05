a47155a15de567a7264ac9e7f80b1d1f93b38342
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 22:59:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
-
+    split(/[.?!]/).reject{|s| s.empty?}.count
   end
 end
