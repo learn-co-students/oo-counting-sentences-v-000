e8cb1e77ee606f7096ed9f19cfa0397451441821
William Hicks <w.hicks917@gmail.com>
2017-12-07 18:17:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    n=0
+    split.each do |word|
+      n += 1 if (word.sentence? || word.question? || word.exclamation?)
+    end
+    n
   end
 end
