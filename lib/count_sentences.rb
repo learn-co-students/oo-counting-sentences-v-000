ce4bd8d6d507f0d125b78c5c1861766b02e3ec3b
Eric Lin <esotericlin@gmail.com>
2017-12-11 20:36:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|sentence| sentence.size < 3}.size
+    self.split(/[.?!/]/).delete_if {|sentence| sentence.size < 3}.size
   end
 end
