ceb7504bb21d7be0e54a66bc44537a2df1e99cbc
EPIC448 <samrey2018@gmail.com>
2018-01-09 06:07:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,8 +29,10 @@ class String
 
   def count_sentences
     self.split(/\.|\?|!/).delete_if{|x| x.size == 0}.length
-  # self a.k.a is used in the class
+  # self a.k.a is used to refer to the class
   # regex  to config /select the right punctuation to fit out array
+   # x can be anything
+   # split is a array of sentences
 # binding.pry
   end
 end
