bf32386e7ce602aefdde9368061df5ed5df64346
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-09 23:20:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
+  # attr_accessor :string
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
+    # self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
