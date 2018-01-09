61b63fff89eb0390fbd6a53eb6b214dce159e534
Sam Kalum <stkalum@gmail.com>
2018-01-09 15:20:46 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,18 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if {|elem| elem.strip == ""}.count
   end
 end
