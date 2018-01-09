ccb265f88b8575dfed381250f89f79d8ecec06c1
EPIC448 <samrey2018@gmail.com>
2018-01-09 06:06:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
-  def question?
 
+  def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).delete_if{|x| x.size == 0}.length
+  # self a.k.a is used in the class
+  # regex  to config /select the right punctuation to fit out array
+# binding.pry
   end
 end
