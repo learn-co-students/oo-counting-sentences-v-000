d484fa0b0e1d0b49f972162e15e62a617f499b2c
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-01-17 01:23:44 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry
+    if self.match(/.*[.]\z/)
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.match(/.*[?]\z/)
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.match(/.*[!]\z/)
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.scan(/\w+[.?!]/)
+    if array.length == 0
+      return 0
+    else
+      return array.length
+    end
   end
 end
