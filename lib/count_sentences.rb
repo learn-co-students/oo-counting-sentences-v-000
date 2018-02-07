7da62c820c919c83ec23a1d62cf3dbe2bf278554
Jayon Kim <jpkim921@gmail.com>
2018-02-07 04:12:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    count = 0
+    self.split(" ").each do |sentence|
+      count += 1 if sentence.sentence? || sentence.question? || sentence.exclamation?
+    end
+    return count
   end
 end
