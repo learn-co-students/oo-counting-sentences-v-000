2e7f1d73ee4b31ef5c1c718bc645c8125203d53a
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-10 02:15:50 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")? true:false
   end
 
   def question?
-
+    self.end_with?("?")? true:false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true:false
   end
 
   def count_sentences
 
+    number_sentences = self.split(/[.?!]/)
+    number_sentences.delete("")
+    number_sentences.count
   end
+#  binding.pry
 end
