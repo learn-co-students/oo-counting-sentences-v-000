e7608b688af1bd0e84276cb33d0fef3d018fe006
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-15 04:14:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    if self.empty?
+      return 0
+    else
+      sentences = self.split(/[.?!]/)
+      sentences.each do |line|
+        if !line.empty? 
+          count += 1
+        end
+      end
+      count
+    end
   end
 end
