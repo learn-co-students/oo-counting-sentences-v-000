3b0abced6f2f53e585d1e3cb9c63ac881ea426b6
Dee Race <hey@deeraceart.com>
2018-02-17 19:44:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,6 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
