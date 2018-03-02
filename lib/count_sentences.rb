20f01061d3ef8332c3b7438dfd30f5fcfb9b3b6f
Rima Jean <rima.jean@gmail.com>
2018-03-02 19:21:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    @words = self.split(/[[\*?!.]$]/)
+    @words.each do |x|
+      if x == ""
+        @words.delete(x)
+      end
+    end
+    @words.length
   end
 end
