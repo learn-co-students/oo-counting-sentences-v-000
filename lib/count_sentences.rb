b534465dd6bc90d5bcc4e07d5a9ab145d54ae076
Aspen James <aspenjames@tqca.org>
2018-03-07 01:25:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/[.]$/) ? true : false
   end
 
   def question?
-
+    self.match(/[?]$/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/[!]$/) ? true : false
   end
 
   def count_sentences
-
+    self.split.grep(/.*[.?!]/).length
   end
 end
