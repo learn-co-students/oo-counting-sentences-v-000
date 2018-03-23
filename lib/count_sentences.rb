47331d157d85f8d8da209064495bf0037130bc0f
Kristin Reddington <kristinreddington@gmail.com>
2018-03-23 17:37:03 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
+    self.end_with?("?") ? true : false
 
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.squeeze("., ?, !").split(/\!|\.|\?/).count #.delete_if { |i| i.empty? }.count
+    
   end
+  sentence = "Helloo....I'm a complicated sentence. I think? Yes, yes I am!!"
+  sentence.count_sentences
 end
