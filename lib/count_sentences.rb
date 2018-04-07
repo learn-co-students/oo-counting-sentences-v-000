d4c5000c2e325a38d9be121efb315fc9afd51665
Travis Williamson <travywill@gmail.com>
2018-04-07 12:48:44 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array_of_sentences = self.split(/[.?!]/)
+    array_of_sentences.delete_if {|string| string == ""}
+    array_of_sentences.count
   end
 end
