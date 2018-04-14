f3e5d8308b4b8f528cd210003f249183b306378b
ecssiah <ecssiah@gmail.com>
2018-04-14 01:01:08 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[\.!?]/).select {|fragment| fragment.length > 0}.count
   end
 end
