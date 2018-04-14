cbe2b968359c446b37e2950d851a5250cfd11dca
ecssiah <ecssiah@gmail.com>
2018-04-14 01:02:30 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[\.!?]/).select {|fragment| fragment.length > 0}.count
+    self.split(/[\.!?]/).select {|w| w.length > 0}.count
   end
 end
