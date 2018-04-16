4940dbf5b82e73a212cda0fbac278a6d471e0394
Everett <everett.bowline@gmail.com>
2018-04-16 12:57:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence_num = []
+    sentence_num = self.split(/[^\.!?]/).reject(&:empty?)
+    sentence_num.count
   end
 end
