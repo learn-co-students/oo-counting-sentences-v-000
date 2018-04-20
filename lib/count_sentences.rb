e322ae7285ab84b6e2d97691199085e0c8e94c19
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-20 19:18:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.match(/\.$/) != nil ? true : false
   end
 
   def question?
-
+    return self.match(/\?$/) != nil ? true : false
   end
 
   def exclamation?
-
+    return self.match(/\!$/) != nil ? true : false
   end
 
   def count_sentences
-
+    self.scan(/[\!\.\?]{1,5}/).length
   end
 end
