8bb87ee2e0783fffe704f20434b04c7ace5b2c9c
Karl Amoguis <ktamoguis@gmail.com>
2018-05-02 02:42:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,42 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
   def sentence?
-    
+    if self[-1] == (".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == ("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == ("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    counter = 0
+    newarray = []
+    newarray = self.split(" ")
+    newarray.each do | words |
+      if words[-1] == "." || words[-1] == "!" || words [-1] == "?"
+        then counter +=1
+      end
+    end
+    counter
   end
+
 end
