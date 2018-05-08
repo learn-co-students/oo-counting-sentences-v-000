85736524e97e7d4bed36c19e78aa3f48cfb27688
Mehwish Aziz <mehwisha@msn.com>
2018-05-08 00:49:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+  ("Hi, my name is Sophie.").end_with?(".")
+  self.end_with?(".")
+
   end
 
   def question?
-
+    ("Hi, my name is Sophie?").end_with?("?")
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    ("Hi, my name is Sophie!").end_with?("!")
+    self.end_with?("!")
   end
 
   def count_sentences
+
+self.squeeze(".?!").split(/[.?!]/).length
 
   end
 end
