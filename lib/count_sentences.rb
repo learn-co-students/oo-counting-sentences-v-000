c25c59f5cb75911ed650513bb86eeefbbee5436e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 18:10:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,6 +3,9 @@ require 'pry'
 class String
 
   def sentence?
+    # this is their (simpler) solution:
+    # self.end_with?(".")
+
     if self[-1] == "."
       true
     else
@@ -27,6 +30,9 @@ class String
   end
 
   def count_sentences
+    # this is their (simpler) solution:
+    # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+
     self.scan(/[\.\!\?](?=\s|$)/).size
   end
 end
