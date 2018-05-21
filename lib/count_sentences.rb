81be511f1140189d9d70233520274d4f6549d832
Karen Flores <karen.ag.flores@gmail.com>
2018-05-21 23:14:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #binding.pry
+    sentence_array = self.split(/[?.!]/)
+    sentence_array.delete_if{|sent| sent == "!" || sent =="." || sent =="?" || sent == ""}
+    #binding.pry
+    sentence_array.count
   end
 end
