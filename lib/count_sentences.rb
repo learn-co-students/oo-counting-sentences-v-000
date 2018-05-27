51d070d5a2d3ead31f03cb8a2ba3bfca1e479f51
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-27 20:16:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    array = self.split(/[\.\!\?]/)
+    array.delete_if {|item| item == ""}
+    array.count
 
   end
 end
