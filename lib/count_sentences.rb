db877984331e60999a6c77b045960c53f03d40e9
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-21 23:11:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # if self == "" || self == 0
+    #   self.delete
+    # else
+    string = self.split(/[.?!]/)
+    string.delete_if { |e| e == "" }
+    return string.count
+    # end
+      # if complex_string.include?("")
+      #   complex_string.delete("")
+      # end
   end
+
 end
