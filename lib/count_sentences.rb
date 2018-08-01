5970fe801ea9b874230b29b9ec3fcac51c75a7cd
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 19:47:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/[!.?]/)
+    i = 0
+    counter = 0
+    # binding.pry
+    count.each do |item|
+      counter += 1 unless item == ""
+    end
+    counter
   end
 end
