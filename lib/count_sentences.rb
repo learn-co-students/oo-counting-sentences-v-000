fc142fa1e69dc8f41c4173e6ae084e21d8a7a558
Kari O'Neal <triadfirewood@gmail.com>
2018-08-10 22:59:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def exclamation?
-
+  if self.end_with?("!")
+    TRUE
+  else
+    FALSE
+    end
   end
 
   def count_sentences
-
+       array = self.split(/[\.?!] /)
+       array.count
   end
+  
 end
