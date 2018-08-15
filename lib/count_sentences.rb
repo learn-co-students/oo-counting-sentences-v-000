c9728865b96c5c980ae0435929206204b095c5d6
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 05:30:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[self.length - 1] == "."
+    return false
   end
 
   def question?
-
+    return true if self[self.length - 1] == "?"
+    return false
   end
 
   def exclamation?
-
+    return true if self[self.length - 1] == "!"
+    return false
   end
 
   def count_sentences
-
+    return self.split(/\.|\!/).count
   end
 end
