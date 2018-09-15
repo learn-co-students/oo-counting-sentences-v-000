7949b825ebbb372fb45a92f123b5311d7b6a941e
Ashley Bland <ashleyjbland@gmail.com>
2018-09-15 16:29:48 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]\s[a-zA-Z]/).count
+#binding.pry
   end
 end
+
+#1. it should split each sentence but not split at ellipsis
