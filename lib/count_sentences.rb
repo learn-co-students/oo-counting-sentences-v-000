9181391d643c7d412ef78285fb4529f48774de98
Sherry Martin <sm674@cornell.edu>
2018-09-20 18:43:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+     return true
+   else
+     return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+    return true
+  else
+    return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+    return true
+  else
+    return false
+    end
   end
 
   def count_sentences
-
+    if self.split(/[.?!]+/).count == 0
+      return 0
+    else
+      self.split(/[.?!]+/).count
+    end
   end
 end
