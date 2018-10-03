e2d2d1d1f714702cc6a78df3efba86c42d926e7e
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2018-10-02 22:02:52 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] === "."
+      return true 
+    else return false
+    end
   end
 
   def question?
-
+    if self[-1] === "?"
+      return true 
+    else return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] === "!"
+      return true 
+    else return false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/\.+|!+|\?+/)
+    sentences.length
   end
 end
