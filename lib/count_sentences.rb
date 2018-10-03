ba340fb7fe7d3e1ace1bf2784d5662255243bd77
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-03 01:03:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+      sentence = self.split(/[\.!?]/)
+        sentence.delete_if {|x| x.length < 2}
+      sentence.count
+    end 
   end
-end
+
+    # sentance = self.scan(/[.!?]/)
+    # sentance.count
+
+#(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
+
+#(/^[A-Z].+[\.!?]$/)
