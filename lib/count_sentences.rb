b77c5409319f9623244d68f9a5db5515e7baba72
Vince Andrew Pimentel <contact@vincep.me>
2018-10-22 03:57:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".") ? true : false
+    #self[-1..-1] == "." ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
+    #self[-1..-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
+    #self[-1..-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    self.scan(/[^\.?!]+[\.?!]*/).length
   end
 end
