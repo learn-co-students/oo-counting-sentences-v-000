a841761f2799faa02b6d65a9438ad4c2eb5eac4c
Tania Whiting <tanwhiting@aol.com>
2018-10-28 02:35:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    marks = ["!","?","."]
+    sentences = []
+    if self.include?("?") || self.include?(".") || self.include?("!")
+          marks.each {|mark| sentences << self.split(mark)}
+            sentences.delete_if do |string|
+                     string == " "
+          end
+    end
+    if sentences == []
+      return 0 
+    end
+    if (self.sentence? || self.question? || self.exclamation?) && (self[-2]=="!" || self[-2]=="." || self[-2]=="?")
+      return sentences.size + 1
+    else
+        sentences.size
+    end
   end
 end
+
