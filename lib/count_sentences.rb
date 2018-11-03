1bed562d9ebfbf593bca3baccb5d6228bbd62180
ktochow1 <ktochow1@binghamton.edu>
2018-11-03 22:07:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    # complete_sentences = []
+    if self.exclamation? == true || self.question? == true || self.sentence? == true
+      self.squeeze('.!?').split(/[?.!]/).count
+      #.reject(&:blank?)
+      #binding.pry
+    else 0
+    end
   end
+
+
 end
