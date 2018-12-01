68b10879dfe3e0d600611f7354cabc34d765a3d6
Arnold Su <arnoldtingsu@gmail.com>
2018-12-01 21:12:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
+    itself.include?(".")
     
   end
 
   def question?
+    itself.include?("?")
 
   end
 
   def exclamation?
+    itself.include?("!")
 
   end
 
   def count_sentences
-
+    itself.squeeze('?!.').count('?!.')
+    
   end
 end
