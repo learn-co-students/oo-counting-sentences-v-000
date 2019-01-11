6cae372dade42cacf7a403927930525f369df608
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 19:19:58 +0000

    Done.

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -1,21 +1,21 @@
-puts describe String do
+ describe String do
   describe "#sentence?" do
     it "returns true if the string that you are calling this method on ends in a period." do
       expect("Hi, my name is Sophie.".sentence?).to eq(true)
     end
 
-  puts  it "returns false if the string that you are calling this method on does NOT end in a period." do 
+    it "returns false if the string that you are calling this method on does NOT end in a period." do
       expect("Hi, my name is Sophie".sentence?).to eq(false)
     end
 
   end
 
-  puts describe "#question?" do
+   describe "#question?" do
     it "returns true if the string that you are calling this method on ends in a question mark." do
       expect("What's your name?".question?).to eq(true)
     end
 
-  puts  it "returns false if the string that you are calling this method on does NOT end in question mark." do
+    it "returns false if the string that you are calling this method on does NOT end in question mark." do
       expect("Happy Halloween!".question?).to eq(false)
     end
 
