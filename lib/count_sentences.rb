117a2730fe6cc5f057b72678929e232c38b5987c
Toni-Ty <antonique2479@yahoo.com>
2019-03-10 02:25:10 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,7 +11,7 @@ class String
 end
 
   def question?
-    self.end_with?("?") #Didn't need to write true/false because the method appears to return it implicitly
+    self.end_with?("?") #Don't need to write true/false because the method appears to return it implicitly
     #   true
     # else
     #   false
@@ -27,9 +27,21 @@ end
   end
 
   def count_sentences
-      self.split(/\.|\?|\!/).delete_if { |x| x.empty?}.size
-      # below is also correct
+    # There are two exclamation point characters in the middle of this set of 3 sentences
+    # ("This is too!!") so it splits the sentence right between the 2 exclam. points,
+    # leaving an empty string that the delete_if method deletes, via the code between the
+    # curly braces.  Last it returns the size of the sentence string.
+    self.split(/[.!?]/).delete_if {|x| x.empty?}.size
+
+      # self.split(/[.!?]/).reject {|x| x.empty?}.size
+      # above and below are also correct
       # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+      #For the code above, it deletes sentences that have a character size of less than
+      # 2 characters and then returns the size of the sentence string.
+      # It appears that a higher number than 2 could've been chosen.
+      #
+
+
 
 
 
