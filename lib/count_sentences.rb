7389876eb339b11b159f5f0605eb8460715aed9b
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-22 16:42:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,6 @@
-require 'pry'
-
-class String
-
-  def sentence?
-    
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+def sentence?
+  if self.end_with?(".")
+    true
+  else false
   end
 end
