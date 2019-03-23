206d221e85f2c1746a7edb8005a44e6ed7c15e40
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-23 05:18:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,6 +1,25 @@
-def sentence?
-  if self.end_with?(".")
-    true
-  else false
+class String
+  def sentence?
+    if self.end_with?(".")
+      true
+    else false
+    end
+  end
+
+  def question?
+    if self.end_with?("?")
+      true
+    else false
+    end
+  end
+
+  def exclamation?
+    if self.end_with?("!")
+      true
+    else false
+    end
+  end
+
+  def count_sentences
   end
 end
