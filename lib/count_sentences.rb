c00813e1c7e40aa34f7cd1a5bd3c92aed230ffdf
Andranique Boone <andranique.boone@gmail.com>
2019-04-09 17:13:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   sentence_count = self.split(/[.?!]/).reject {|string| string.empty?} # reject {|item| block } → new_ary
+#Returns a new array containing the items in self for which the given block is not true. The ordering of non-rejected elements is maintained.
+   return sentence_count.length
   end
 end
