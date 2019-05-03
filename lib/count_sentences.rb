ea28b74d840188207447cf815980f106bb3f98e4
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-05-03 03:55:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,15 @@ class String
   end
 
   def count_sentences
-    delimiters = [". ","? ","! "]
-    self.split(Regexp.union(delimiters)).count
+    ## internet researched solution
+    #delimiters = [". ","? ","! "]
+    #self.split(Regexp.union(delimiters)).count
+
+    #Flatiron solution
+    #self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    #regex, enclosed by fwd slashes, checks for (escaped) period or question mark or exclamation point
+    #delete_if method takes a block where w holds the split values, iterating through to check that there is 2 or more chars
+    #size counts the sentences
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
