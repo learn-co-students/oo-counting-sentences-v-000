d2e00d1d3d04f30d681ed8a282902fe61586a5ea
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-23 15:25:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,32 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      true
-    else
-      false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      true
-    else
-      false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
-    sentence_number = self.split(/[.!?]+/)
-    sentence_number.count
-
+    self.split(/[.!?]+/).length
   end
+
 end
